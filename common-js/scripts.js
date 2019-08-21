944af337ed2d058bb079c739f76e1417b0878c75
ourwedding2020 <53875890+ourwedding2020@users.noreply.github.com>
2019-08-20 17:16:13 -0700

    Update scripts.js

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -1,58 +1,58 @@
 
-	
+
 (function ($) {
 
     "use strict";
 
 	// JQUERY LIGHT BOX
-	
+
 	if ( $.isFunction($.fn.fluidbox) ) {
 		$('a').fluidbox();
 	}
 
-	
+
 	$('a[href="#"]').on('click', function(event){
 		return;
 	});
-	
-	// COUNTDOWN TIME 
-	
+
+	// COUNTDOWN TIME
+
 	countdownTime();
-	
-	
+
+
 	$('[data-nav-menu]').on('click', function(event){
-		
+
 		var $this = $(this),
 			visibleHeadArea = $this.data('nav-menu');
-		
+
 		$(visibleHeadArea).toggleClass('visible');
-		
+
 	});
-	
-	
+
+
 	var winWidth = $(window).width();
 	dropdownMenu(winWidth);
-	
+
 	$(window).on('resize', function(){
 		dropdownMenu(winWidth);
-		
+
 	});
-	
+
 	// Circular Progress Bar
-	
+
 	var isAnimated = false;
-	
-	
+
+
 })(jQuery);
 
 
 
 function countdownTime(){
-	
+
 	if(isExists('#clock')){
 		$('#clock').countdown('2020/01/04', function(event){
 			var $this = $(this).html(event.strftime(''
-				+ '<div class="time-sec"><span class="title">%D</span> days </div>'
+				+ '<div class="time-sec"><span class="title" style="color:black">%D</span> days </div>'
 				+ '<div class="time-sec"><span class="title">%H</span> hours </div>'
 				+ '<div class="time-sec"><span class="title">%M</span> minutes </div>'
 				+ '<div class="time-sec"><span class="title">%S</span> seconds </div>'));
@@ -60,37 +60,37 @@ function countdownTime(){
 	}
 }
 function dropdownMenu(winWidth){
-	
+
 	if(winWidth > 767){
-		
+
 		$('.main-menu li.drop-down').on('mouseover', function(){
 			var $this = $(this),
 				menuAnchor = $this.children('a');
-				
+
 			menuAnchor.addClass('mouseover');
-			
+
 		}).on('mouseleave', function(){
 			var $this = $(this),
 				menuAnchor = $this.children('a');
-				
+
 			menuAnchor.removeClass('mouseover');
 		});
-		
+
 	}else{
-		
+
 		$('.main-menu li.drop-down > a').on('click', function(){
-			
+
 			if($(this).attr('href') == '#') return false;
 			if($(this).hasClass('mouseover')){ $(this).removeClass('mouseover'); }
 			else{ $(this).addClass('mouseover'); }
 			return false;
 		});
 	}
-	
+
 }
 
 function isExists(elem){
-	if ($(elem).length > 0) { 
+	if ($(elem).length > 0) {
 		return true;
 	}
 	return false;
@@ -236,7 +236,7 @@ function initMap() {
 		zoom: 4,
 		center: uluru
     });
-		
+
 	var image = 'images/google-marker.png';
 	var marker = new google.maps.Marker({
 		position: uluru,
