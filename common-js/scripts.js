b0c1ebe26fa6b089b1f0054a28d1f4ad857dd2e7
ourwedding2020 <53875890+ourwedding2020@users.noreply.github.com>
2019-08-20 17:41:34 -0700

    Update scripts.js

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -52,7 +52,7 @@ function countdownTime(){
 	if(isExists('#clock')){
 		$('#clock').countdown('2020/01/04', function(event){
 			var $this = $(this).html(event.strftime(''
-				+ '<div class="time-sec"><span class="title" style="color:black">%D</span> days </div>'
+				+ '<div class="time-sec"><span class="title">%D</span> days </div>'
 				+ '<div class="time-sec"><span class="title">%H</span> hours </div>'
 				+ '<div class="time-sec"><span class="title">%M</span> minutes </div>'
 				+ '<div class="time-sec"><span class="title">%S</span> seconds </div>'));
